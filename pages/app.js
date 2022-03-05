c272635389b806943b3bc95df9a3d9e2d12031fd
Siddhant Sarkar <siddhant.sarkar999@gmail.com>
2022-03-05 14:20:22 +0530

    backend

--- pages/app.js
+++ pages/app.js
@@ -7,6 +7,9 @@ app.get('/',(req,res)=>{
   // res.sendFile("./index.html") ;
 res.sendFile(path.join(__dirname,'index.html'));
 })
+app.get('/login',(req,res)=>{
+  res.status(200).sendFile(path.join(__dirname,'login.html'));
+})
 app.listen(5050,()=>{
   console.log('Server is listening to 5050');
 })
